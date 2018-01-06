d6fe5bd14979d8a834bb41e7605552da46e122a5
Mark Entingh <entingh@gmail.com>
2018-01-06 06:10:20 -0800

    deleted unused methods & class files

--- Random.cs
+++ Random.cs
@@ -12,11 +12,7 @@ namespace Crypto
             m_rng.GetBytes(buffer, offset, count);
             return buffer;
         }
-        public static byte[] GetBytes(byte[] buffer) => GetBytes(buffer, 0, buffer.Length);
         public static byte[] GetBytes(int count) => GetBytes(new byte[count], 0, count);
-        public static int GetInt32() => BitConverter.ToInt32(GetBytes(sizeof(int)), 0);
-        public static long GetInt64() => BitConverter.ToInt64(GetBytes(sizeof(long)), 0);
         public static uint GetUInt32() => BitConverter.ToUInt32(GetBytes(sizeof(uint)), 0);
-        public static ulong GetUInt64() => BitConverter.ToUInt64(GetBytes(sizeof(ulong)), 0);
     }
 }
